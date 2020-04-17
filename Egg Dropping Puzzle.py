b6d492d1cd7b20d2abd7e89a34d2bab465d17341
sagnikpal10 <sagnik.pal2017@vitstudent.ac.in>
2020-04-17 11:44:47 +0530

    Egg Dropping Puzzle update

--- Egg Dropping Puzzle.py
+++ Egg Dropping Puzzle.py
@@ -25,9 +25,35 @@ def eggDrop(n, k):
   
     return ans
 
+# Function to get minimum number of trials needed in worst 
+# case with n eggs and k floors 
+def eggDrop_dp(n, k): 
+    # A 2D table where entery eggFloor[i][j] will represent minimum 
+    # number of trials needed for i eggs and j floors. 
+    eggFloor = [[0 for x in range(k+1)] for x in range(n+1)] 
+
+    for i in range(1, n+1): 
+        eggFloor[i][1] = 1
+        eggFloor[i][0] = 0
+
+    for j in range(1, k+1): 
+        eggFloor[1][j] = j 
+   
+    for i in range(2, n+1): 
+        for j in range(2, k+1): 
+            eggFloor[i][j] = sys.maxsize 
+            for x in range(1, j+1): 
+                res = 1 + max(eggFloor[i-1][x-1], eggFloor[i][j-x]) 
+                eggFloor[i][j] = min(eggFloor[i][j], res) 
+  
+    # eggFloor[n][k] holds the result 
+    return eggFloor[n][k] 
+
+
 n = 2
 k = 10
 print("Minimum number of trials in worst case with", n, "eggs and", k, "floors is", eggDrop(n, k)) 
+print("Minimum number of trials in worst case with", n, "eggs and", k, "floors is", eggDrop_dp(n, k)) 
 
 
 
